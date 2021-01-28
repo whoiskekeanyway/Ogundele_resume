9d0a8569f300777dc4a6849e7eb998a12a8a7dc2
whoiskekeanyway <whoiskekeanyway@outlook.com>
2021-01-28 04:17:37 +0200

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 //Get the button
-let mybutton = document.getElementById("myBtn");
+var mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -17,3 +19,4 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
